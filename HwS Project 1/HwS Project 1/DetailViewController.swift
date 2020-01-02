b41cc2122ab82d9095a0892f0fb6dfdc5fb983d1
Heather Mason <heatem@gmail.com>
2020-01-01 21:38:52 -0500

    add detail screen, enlarge main screen title

---
+++ HwS Project 1/HwS Project 1/DetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  HwS Project 1
+//
+//  Created by Heather Mason on 1/1/20.
+//  Copyright © 2020 Heather Mason. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        title = selectedImage
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
