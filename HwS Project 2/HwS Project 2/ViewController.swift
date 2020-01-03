478ededdc672c0519989ed240a6e40bbe68234b3
Heather Mason <heatem@gmail.com>
2020-01-03 16:34:49 -0500

    update flag buttons, add border

--- HwS Project 2/HwS Project 2/ViewController.swift
+++ HwS Project 2/HwS Project 2/ViewController.swift
@@ -13,11 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        let buttons = [button1, button2, button3]
+        
+        for button in buttons {
+            button?.layer.borderWidth = 1
+            button?.layer.borderColor = UIColor.lightGray.cgColor
+        }
 
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
 
 }
 
